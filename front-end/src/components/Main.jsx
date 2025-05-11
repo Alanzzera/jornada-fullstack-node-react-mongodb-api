b68876895e8d67ee100040ec9f2b97a3a7cc2431
Alan Griebler <alaneduardotp@hotmail.com>
2025-05-11 20:41:20 -0300

    deploy

--- front-end/src/components/Main.jsx
+++ front-end/src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ItemList from "./itemList";
+import ItemList from "./ItemList";
 import { artistsArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
