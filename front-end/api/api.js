ee5bf3526e68a5348abc2b951bee91be9d3c5008
Alan Griebler <alaneduardotp@hotmail.com>
2025-05-11 21:03:45 -0300

    deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
-// const URL = process.env.NODE_ENV === "development"
-//   ? "http://localhost:3000/api"
-//   : process.env.URL;
+dotenv.config();
 
-const URL = process.env.URL;
+const URL = process.env.NODE_ENV === "development"
+  ? "http://localhost:3000/api"
+  : process.env.URL;
 
 const responseArtists = await axios.get(`${URL}/artists`);
 export const artistsArray = responseArtists.data;
